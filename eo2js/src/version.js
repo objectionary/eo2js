cd976dbc38c506be3efe405c0a9bc9fe9966cdca
rultor <me@rultor.com>
2024-06-01 23:37:14 +0000

    set version to 0.0.7

--- eo2js/src/version.js
+++ eo2js/src/version.js
@@ -1,6 +1,6 @@
 // The values here are replaced automatically by the .rultor.yml script,
 // at the "release" pipeline:
 module.exports = {
-  what: '0.0.0',
-  when: '0000-00-00'
+  what: '0.0.7',
+  when: '2024-06-01'
 };
