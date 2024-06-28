91d45094061f303a68ac46644db7f9a5caf16cb5
maxonfjvipon <mtrunnikov@gmail.com>
2024-06-28 15:36:08 +0300

    fix(#93): message

--- eo2js/src/commands/test.js
+++ eo2js/src/commands/test.js
@@ -9,16 +9,21 @@ const {execSync} = require('child_process')
 const test = function(options) {
   options = {...program.opts(), ...options}
   const dir = path.resolve(options.target, options.project)
-  execSync(
-    [
-      'node ./node_modules/mocha/bin/mocha.js .',
-      '--recursive',
-      '--exclude "__main__.js"',
-      '--exclude "node_modules/**"',
-      ...options.exclude.split(',').map((glob) => `--exclude "${glob}"`)
-    ].join(' '),
-    {stdio: 'inherit', cwd: dir}
-  )
+  try {
+    execSync(
+      [
+        'node ./node_modules/mocha/bin/mocha.js .',
+        '--recursive',
+        '--exclude "__main__.js"',
+        '--exclude "node_modules/**"',
+        ...options.exclude.split(',').map((glob) => `--exclude "${glob}"`)
+      ].join(' '),
+      {stdio: 'inherit', cwd: dir}
+    )
+  } catch (ex) {
+    console.error('Failed to execute mochajs', ex.message)
+    throw ex
+  }
 }
 
 module.exports = test
