0eda4ef5f7257d7257c0c53caa53e2565b23ca12
HaidarJbeily7 <h.jbeily@innopolis.university>
2024-11-24 00:22:58 +0300

    feat: implement dir walkjs

--- eo2js-runtime/test/objects/org/eolang/fs/dir$walk.test.js
+++ eo2js-runtime/test/objects/org/eolang/fs/dir$walk.test.js
@@ -2,37 +2,37 @@ const assert = require('assert');
 const dir$walk = require('../../../../../temp/objects/org/eolang/fs/dir$walk');
 const {STRING} = require('../../../../../temp/runtime/types');
 const dataized = require('../../../../../temp/runtime/dataized');
-const at_string = require('../../../../../temp/runtime/attribute/at-string');
+const at_string = require('../../../../runtime/attribute/at-string');
 const fs = require('fs');
 const path = require('path');
 
 describe('dir$walk', function() {
-  const testDir = path.resolve(__dirname, '../../../../../temp/test-dir');
+  const directory = path.resolve(__dirname, '../../../../../temp/test-dir');
   
   beforeEach(function() {
-    if (fs.existsSync(testDir)) {
-      fs.rmSync(testDir, {recursive: true});
+    if (fs.existsSync(directory)) {
+      fs.rmSync(directory, {recursive: true});
     }
-    fs.mkdirSync(testDir, {recursive: true});
-    fs.writeFileSync(path.join(testDir, 'file1.txt'), '');
-    fs.writeFileSync(path.join(testDir, 'file2.js'), '');
-    fs.mkdirSync(path.join(testDir, 'subdir'));
-    fs.writeFileSync(path.join(testDir, 'subdir/file3.txt'), '');
+    fs.mkdirSync(directory, {recursive: true});
+    fs.writeFileSync(path.join(directory, 'file1.txt'), '');
+    fs.writeFileSync(path.join(directory, 'file2.js'), '');
+    fs.mkdirSync(path.join(directory, 'subdir'));
+    fs.writeFileSync(path.join(directory, 'subdir/file3.txt'), '');
   });
 
   afterEach(function() {
-    fs.rmSync(testDir, {recursive: true});
+    fs.rmSync(directory, {recursive: true});
   });
 
   it('should find files matching glob pattern', function() {
     const walk = dir$walk();
-    walk.attrs['glob'] = at_string(path.join(testDir, '**/*.txt'));
+    walk.attrs['glob'] = at_string(path.join(directory, '**/*.txt'));
     const result = dataized(walk, STRING).split('\n').sort();
     assert.deepEqual(
       result,
       [
-        path.join(testDir, 'file1.txt'),
-        path.join(testDir, 'subdir/file3.txt')
+        path.join(directory, 'file1.txt'),
+        path.join(directory, 'subdir/file3.txt')
       ].sort()
     );
   });
