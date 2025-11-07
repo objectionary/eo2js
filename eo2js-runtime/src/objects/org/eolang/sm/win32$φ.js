7f174e16b00581a02e33e7ba27ee23c1e055fc12
skulidropek <legovlad818@gmail.com>
2025-11-07 23:47:57 +0400

    fix(#117): Implement win32$φ atom
    The win32$φ atom now properly detects the Windows platform by returning
    the result of \`process.platform === 'win32'\`, replacing the previous
    unimplemented stub that threw an error.

--- eo2js-runtime/src/objects/org/eolang/sm/win32$φ.js
+++ eo2js-runtime/src/objects/org/eolang/sm/win32$φ.js
@@ -3,21 +3,15 @@
 
 const object = require('../../../../runtime/object')
 const {LAMBDA} = require('../../../../runtime/attribute/specials');
-const ErFailure = require('../../../../runtime/error/ErFailure');
 
 /**
- * Win32.φ.
- * @return {Object} - Win32.φ object
- * @todo #3:30min Implement win32$φ atom. We need to implement the atom and make sure it
- *  works. For the details of implementation check the Java analogue on
- *  https://github.com/objectionary/eo/tree/master/eo-runtime/src/main/java/EOorg/EOeolang
+ * Win32.φ - checks if the current platform is Windows.
+ * @return {Object} - Win32.φ object that returns a boolean on dataization
  */
 const win32$φ = function() {
   const obj = object('win32$φ')
   obj.assets[LAMBDA] = function(_) {
-    throw new ErFailure(
-      `Atom win32$φ is not implemented yet`
-    )
+    return process.platform === 'win32'
   }
   return obj
 }
