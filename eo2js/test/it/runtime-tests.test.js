1d2f2edee4a3be61b883e8917f50f80942d3f117
Dev <dev@example.com>
2026-02-05 14:02:06 +0000

    test(win32): exclude win32 tests on non-windows

--- eo2js/test/it/runtime-tests.test.js
+++ eo2js/test/it/runtime-tests.test.js
@@ -27,6 +27,7 @@ const exclude = [
   'math/real-tests',
   'structs/list-tests',
   'sys/posix-tests',
+  ...(process.platform === 'win32' ? [] : ['sys/win32-tests']),
   'txt/regex-tests',
   'txt/sprintf-tests',
   'txt/sscanf-tests',
